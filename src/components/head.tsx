0bd4ef564bdd9a91999f2a1527cf453290ffeb88
Maciej Trębacz <maciej@trebacz.org>
2023-04-02 22:26:07 +0200

    Remove example code, add tailwind

---
+++ src/components/head.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export default function AppHead() {
+  return (
+    <Head>
+      <title>Create Next App</title>
+      <meta name="description" content="GPT-Dev app" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>    
+  )
+}
