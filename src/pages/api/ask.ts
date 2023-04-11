cf870b8ffbe3c4c63df6540dae7c84534f61a5e2
kovenz0r <koven_au@hotmail.com>
2023-04-11 23:04:59 +0800

    Header error fix
    error - Error [ERR_HTTP_HEADERS_SENT]: Cannot set headers after they are sent to the client
        at new NodeError (node:internal/errors:399:5)
        at ServerResponse.setHeader (node:_http_outgoing:663:11)

--- src/pages/api/ask.ts
+++ src/pages/api/ask.ts
@@ -1,38 +1,46 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { Assistant } from '@/lib/server/assistant'
-import { Message } from '@/types'
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { Assistant } from '@/lib/server/assistant';
+import { Message } from '@/types';
+import { ServerResponse } from 'http';
 
 interface AskRequest {
-  basePath: string
-  description: string
-  text: string
-  messages: Message[]
+  basePath: string;
+  description: string;
+  text: string;
+  messages: Message[];
 }
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method !== "POST") {
-    res.status(405).send("Method Not Allowed: " + req.method);
+  if (req.method !== 'POST') {
+    res.status(405).send('Method Not Allowed: ' + req.method);
     return;
   }
 
   const params = req.body as AskRequest;
-  const assistant = new Assistant(params.basePath, params.description, params.messages, (item) => {
-    // Sends conversation items via Server-Sent Events back to the client
-    res.write(`data: ${JSON.stringify(item)}\n\n`);
+  const sseRes = new ServerResponse(req); // Create a new ServerResponse object for SSE
+
+  // Set up error event listener
+  sseRes.on('error', (error) => {
+    console.error(error);
+    res.status(500).send('Internal Server Error'); // Use the original 'res' object to send an error response
   });
 
-  res.setHeader("Content-Type", "text/event-stream");
-  res.setHeader("Cache-Control", "no-cache");
-  res.setHeader("Connection", "keep-alive");
-  res.flushHeaders();
+  const assistant = new Assistant(params.basePath, params.description, params.messages, (item) => {
+    // Sends conversation items via Server-Sent Events back to the client
+    sseRes.write(`data: ${JSON.stringify(item)}\n\n`);
+  });
+
+  sseRes.setHeader('Content-Type', 'text/event-stream');
+  sseRes.setHeader('Cache-Control', 'no-cache');
+  sseRes.setHeader('Connection', 'keep-alive');
+  sseRes.flushHeaders();
 
   try {
     await assistant.prompt(params.text);
-    res.write("event: close\n\n");
-    res.end();
+    sseRes.write('event: close\n\n');
+    sseRes.end();
   } catch (error) {
     console.error(error);
-    res.status(500).send("Internal Server Error");
+    res.status(500).send('Internal Server Error'); // Use the original 'res' object to send an error response
   }
 }
-
